37a5bcbb6dc4bda300de9651f5c1b464b975ca2d
HiLeeS <azs8017@naver.com>
2025-03-27 21:54:26 +0900

    app.js 수정

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import OnMeeting from './components/OnMeeting';
 import CodeInput from "./components/CodeInput";
 import CodeCreate from "./components/CodeCreate";
