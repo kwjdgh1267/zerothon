b8a3fe20e822837ed8dcd5fd58d52a5f8c9e9849
HiLeeS <azs8017@naver.com>
2025-03-27 15:58:26 +0900

    add main.js, MeetingDetail.js  기본 기능 구현, 수정 해야함

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Main from "./pages/Main";
+import MeetingDetail from "./pages/MeetingDetail";  // MeetingDetail.js 가져오기
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router> {/* App.js에서 Router만 한 번 감쌈 */}
+      <Routes>
+        <Route path="/" element={<Main />} />
+        <Route path="/meeting/:id" element={<MeetingDetail />} />
+      </Routes>
+    </Router>
   );
 }
 
