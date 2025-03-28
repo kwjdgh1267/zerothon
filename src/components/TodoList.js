2c255d272df682a9450f6c29928c868cec1b327c
seoyeeon <shift2013@naver.com>
2025-03-28 17:24:27 +0900

    first front

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,10 +1,10 @@
 import React from "react";
 import { Checkbox } from "./ui/checkbox";
 
-const TodoList = ({ todoItems }) => {
+const TodoList = ({ todoItems = [] }) => {
   return (
     <div className="mt-8 space-y-6">
-      {todoItems.map((item, index) => (
+      {todoItems?.map((item, index) => (
         <div key={index} className="flex items-start">
           <Checkbox id={`todo-${index}`} className="mt-1 rounded border-[#c4c4c4]" />
           <div className="ml-5 flex justify-between w-full">
